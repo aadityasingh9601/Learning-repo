79ece4b355e064b21154a3fbfd76ca0426b6aabb
Aaditya Singh <aadityasingh9601@gmail.com>
2025-06-20 20:37:16 +0530

    Created a pub-subs like system for stock updates

---  stateful,singleton,pub-subs/src/logger.ts
+++  stateful,singleton,pub-subs/src/logger.ts
@@ -5,3 +5,6 @@ export function startLogger() {
     GameManager.getInstance().log();
   }, 3000);
 }
+
+// Similar to the function here, you can store the state of backend into the database, as state updates, you can
+//push those changes to a queue and then the queue will keep updating the state in the database.
