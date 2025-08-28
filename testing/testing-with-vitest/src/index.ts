8636244cd04c69c72849617d1a4e0ced8f261d8d
Aaditya Singh <aadityasingh9601@gmail.com>
2025-08-28 20:00:08 +0530

    Added CI&CD pipeline, spying and mocking return values

--- testing/testing-with-vitest/src/index.ts
+++ testing/testing-with-vitest/src/index.ts
@@ -7,11 +7,18 @@ app.use(express.json());
 
 app.post("/sum", async (req, res) => {
   const { a, b } = req.body;
+
+  if (a > 1000000 || b > 1000000) {
+    res.status(422).json({
+      message: "Sorry we don't support big numbers!",
+    });
+  }
+
   let answer = a + b;
 
   //Put data into the database.
 
-  await prismaClient.sum.create({
+  const request = await prismaClient.sum.create({
     data: {
       a: a,
       b: b,
@@ -19,7 +26,10 @@ app.post("/sum", async (req, res) => {
     },
   });
 
+  console.log(request);
+
   res.json({
     answer,
+    id: request.id,
   });
 });
